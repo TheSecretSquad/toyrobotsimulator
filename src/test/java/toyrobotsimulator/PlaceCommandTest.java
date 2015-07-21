acd3d0b319243c28698773018cb217994150f592
Pete DiSalvo <pdisalvo486@gmail.com>
2015-07-21 00:12:31 -0500

    Trial and error. Minimal change.

--- src/test/java/toyrobotsimulator/PlaceCommandTest.java
+++ src/test/java/toyrobotsimulator/PlaceCommandTest.java
@@ -3,7 +3,6 @@ package toyrobotsimulator;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.verify;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -12,19 +11,11 @@ import org.mockito.runners.MockitoJUnitRunner;
 @RunWith(MockitoJUnitRunner.class)
 public class PlaceCommandTest {
 
-	private PlaceCommand placeCommandWithCorrectParameters;
-	@Mock
-	private CommandParameters correctCommandParameters;
+	private PlaceCommand placeCommand;
 	@Mock
 	private PlaceAction placeAction;
 
-	@Before
-	public void setUp() throws Exception {
-		correctCommandParameters = createCommandParameters("1", "2", "NORTH");
-		placeCommandWithCorrectParameters = new PlaceCommand(correctCommandParameters, placeAction);
-	}
-
-	private CommandParameters createCommandParameters(final String ... parameters) {
+	private CommandParameters commandParameters(final String ... parameters) {
 		return new CommandParameters() {
 			@Override
 			public void listTo(final CommandParameterReceiver commandParameterReceiver) {
@@ -34,9 +25,14 @@ public class PlaceCommandTest {
 		};
 	}
 	
+	private void setupPlaceCommandWith(final CommandParameters commandParameters) {
+		placeCommand = new PlaceCommand(commandParameters, placeAction);
+	}
+	
 	@Test
-	public void WhenExecutingCommand_WithCorrectParameters_ShouldCallPlace() {
-		placeCommandWithCorrectParameters.execute();
+	public void WhenExecutingCommand_WithCorrectParameters_ShouldCallPlaceWithPositionAndDirection() {
+		setupPlaceCommandWith(commandParameters("1", "2", "NORTH"));
+		placeCommand.execute();
 		verify(placeAction).place(eq(new Position(1, 2)), eq(new Direction("NORTH")));
 	}
 }
